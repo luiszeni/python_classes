73b6dacef9495b70fec404c819ca584cf88616a6
Luis Zeni <zeni@Eye-of-Sauron.local>
2017-04-20 18:56:04 -0300

    adicionado os vídeos com a solução de alguns dos exercícios

--- solutions/cap03/ex10.py
+++ solutions/cap03/ex10.py
@@ -1,4 +1,8 @@
 
-valor = 1
-while valor % 2 == 1:
-    valor = int(raw_input("digite um valor "))
+n = int(raw_input("Digite um numero"))
+soma = 0;
+
+for i in range(1, n+1):
+    soma = soma + i;
+    
+print "o valor da soma e" +  str(soma);
